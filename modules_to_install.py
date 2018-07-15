94e41b0af547f2a9957a319aedfde65b50eba90d
Stefan Hiebl <stefan.hiebl@gmx.com>
2018-07-15 13:42:46 -0500

    Made the modules file actually runable

--- modules_to_install.py
+++ modules_to_install.py
@@ -1,11 +1,8 @@
-'''
-socket
-ipaddress
-requests
-sys
-bs4
-time
-termcolor
-scapy
-random
-'''
+import os
+
+modules = "ipaddress, requests, beautifulsoup4, termcolor, scapy"
+modules_split = modules.split(", ")
+
+for i in modules_split:
+    print("\n Installing module named \'" + i + "\'")
+    os.system("pip3 install " + i)
